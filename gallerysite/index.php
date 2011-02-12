56afe68e17275742e617e78f959e491549e5c481
annaship <ashipunova3@gmail.com>
2011-02-12 11:58:00 -0500

    page size again and left menu added

--- gallerysite/index.php
+++ gallerysite/index.php
@@ -8,20 +8,61 @@ Where is "position: fixed; top: 0px; left: 0px; height: 0px; width: 0px; z-index
 -->
 <div id="container">
   <div id="header">
-<!--    <div class="two_columns">-->
-      <div class="left_column">
+    <div class="left_column">
+    </div>
+    <div class="right_column">
+      <div id="title_img">
+        <img alt="" src="images/header/header.jpg"/>
       </div>
-      <div class="right_column">
-        <div id="title_img">
-          <img alt="" src="images/header/header.jpg"/>
-        </div>
-      </div>
-<!--    </div>-->
+    </div>
   </div>
   <div id="content">
-<!--    <div class="two_columns">-->
       <div class="left_column">
-<!--text home thumbs tab-->
+        <div id="left_menu">
+          <ul>
+            <ul id="first_level">
+              <li>Galleries:</li>
+              <ul id="second_level">
+                <li>Casalinghe:</li>
+                <ul id="third_level"><li>Italian Housewives</li>
+                </ul>
+              </ul>
+              <ul id="second_level">
+                <li>Panoramas:</li>
+                <ul id="third_level"><li>The Life in Death in Venice</li>
+                    <li>Venetian Spaces: Openings</li>
+                </ul>
+              </ul>
+              <ul id="second_level">
+                <li>Venetian Laundry</li>
+                <ul id="third_level"><li>&amp; the Art of Hanging it</li>
+                </ul>
+              </ul>
+              <ul id="second_level">
+                <li>Galleggianti:</li>
+                <ul id="third_level">
+                  <li>Floating Photographic</li>
+                  <li>Emulsions</li>
+                </ul>
+              </ul>
+              <ul id="second_level">
+                <li>The Reitia Project:</li>
+                <ul id="third_level">
+                  <li>Women&nbsp;&amp; Fabric</li>
+                  <li>in Venetian Art</li>
+                </ul>
+              </ul>
+            </ul>
+            <ul id="first_level">
+              <li>Workshops:</li>
+              <ul id="second_level">
+                <li>Venice, Italy</li>
+                <ul id="third_level"><li>September 17-28, 2011</li>
+                </ul>
+              </ul>
+            </ul>
+          </ul>
+        </div>
       </div>
       <div class="right_column">
         <div id="big_img">
