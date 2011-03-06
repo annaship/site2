281df7927e2ceb40436f8128a0dd9b7c802760bd
annaship <ashipunova3@gmail.com>
2011-03-05 20:02:04 -0500

    venetian laundry 960

--- gallerysite/index.php
+++ gallerysite/index.php
@@ -6,7 +6,6 @@
 <div id="content" class="container_12">
 
 <div id="left-content" class="grid_4 ">
-  <div id="left-content-div">
   <div id="menu">
       <div id="left_menu">
 <!--          <ul id="left_menu">-->
@@ -53,7 +52,6 @@
 <!--          </ul>-->
       </div>
     </div>
-  </div>
 </div>
 
 <div id="right-content" class="grid_8 ">
--- gallerysite/spaces.php
+++ gallerysite/spaces.php
@@ -1,45 +1,48 @@
 <?php
   include_once 'template.php';
-  template_first_part("");
+  template_first_part("Title here");
 ?>
-<div id="gallery_page">
-  <div id="content">
-    <div class="left_column">
-      <table id="thumbs">
-        <tr>
-         <td><img src="images/galleries/spaces/01_spc_thumb.jpg"
-                  alt="Misericordia"/></td>
-         <td><img src="images/galleries/spaces/02_spc_thumb.jpg"
-                  alt="Glass globes"/></td>
-        </tr>
-        <tr>
-         <td><img src="images/galleries/spaces/03_spc_thumb.jpg"
-                  alt="Papafava"/></td>
-         <td><img src="images/galleries/spaces/04_spc_thumb.jpg"
-                  alt="Arsenale"/></td>
-        </tr>
-        <tr>
-         <td><img src="images/galleries/spaces/05_spc_thumb.jpg"
-                  alt="Troughs"/></td>
-         <td><img src="images/galleries/spaces/06_spc_thumb.jpg"
-                  alt="Bathroom"/></td>
-        </tr>
-        <tr>
-         <td><img src="images/galleries/spaces/07_spc_thumb.jpg"
-                  alt="Nativity"/></td>
-         <td><img src="images/galleries/spaces/08_spc_thumb.jpg"
-                  alt="Evangelista sculpture"/></td>
-        </tr>
-      </table>
-    </div>
-    <div class="right_column">
-      <div id="galleries_title">The Life in Death in Venice</div>
-      <div id="big_img_one">
-        <img id="BigOne" src="images/galleries/spaces/01_spc.jpg" alt="Misericordia"/>
-      </div>
-    </div>
+
+<div id="content" class="container_12">
+
+<div id="left-content" class="grid_4 ">
+    <table id="thumbs">
+      <tr>
+       <td><img src="images/galleries/spaces/01_spc_thumb.jpg"
+                alt="Misericordia"/></td>
+       <td><img src="images/galleries/spaces/02_spc_thumb.jpg"
+                alt="Glass globes"/></td>
+      </tr>
+      <tr>
+       <td><img src="images/galleries/spaces/03_spc_thumb.jpg"
+                alt="Papafava"/></td>
+       <td><img src="images/galleries/spaces/04_spc_thumb.jpg"
+                alt="Arsenale"/></td>
+      </tr>
+      <tr>
+       <td><img src="images/galleries/spaces/05_spc_thumb.jpg"
+                alt="Troughs"/></td>
+       <td><img src="images/galleries/spaces/06_spc_thumb.jpg"
+                alt="Bathroom"/></td>
+      </tr>
+      <tr>
+       <td><img src="images/galleries/spaces/07_spc_thumb.jpg"
+                alt="Nativity"/></td>
+       <td><img src="images/galleries/spaces/08_spc_thumb.jpg"
+                alt="Evangelista sculpture"/></td>
+      </tr>
+    </table>
+</div>
+
+<div id="right-content" class="grid_8 ">
+  <div id="galleries_title">The Life in Death in Venice</div>
+  <div id="big_img_one">
+    <img id="BigOne" src="images/galleries/spaces/01_spc.jpg" alt="Misericordia"/>
   </div>
 </div>
+
+</div>
+
 <?php
   template_last_part()
 ?>
