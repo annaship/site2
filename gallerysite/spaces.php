e600b6528d021a501df8b8245998fa8dd43b5607
annaship <ashipunova3@gmail.com>
2011-03-07 08:16:46 -0500

    vertical line; new img for spaces

--- gallerysite/spaces.php
+++ gallerysite/spaces.php
@@ -6,29 +6,35 @@
 <div id="content" class="container_12">
 
 <div id="left-content" class="grid_4 ">
+  <div id="left-title">
+  <div id="left-title-big">
+    Venetian Spaces:
+  </div>
+  <div id="left-title-small">Panoramas</div>
+  </div>
     <table id="thumbs">
       <tr>
-       <td><img src="images/galleries/spaces/01_spc_thumb.jpg"
+       <td><img src="images/galleries/spaces/01_space_thumb.jpg"
                 alt="Misericordia"/></td>
-       <td><img src="images/galleries/spaces/02_spc_thumb.jpg"
+       <td><img src="images/galleries/spaces/02_space_thumb.jpg"
                 alt="Glass globes"/></td>
       </tr>
       <tr>
-       <td><img src="images/galleries/spaces/03_spc_thumb.jpg"
+       <td><img src="images/galleries/spaces/03_space_thumb.jpg"
                 alt="Papafava"/></td>
-       <td><img src="images/galleries/spaces/04_spc_thumb.jpg"
+       <td><img src="images/galleries/spaces/04_space_thumb.jpg"
                 alt="Arsenale"/></td>
       </tr>
       <tr>
-       <td><img src="images/galleries/spaces/05_spc_thumb.jpg"
+       <td><img src="images/galleries/spaces/05_space_thumb.jpg"
                 alt="Troughs"/></td>
-       <td><img src="images/galleries/spaces/06_spc_thumb.jpg"
+       <td><img src="images/galleries/spaces/06_space_thumb.jpg"
                 alt="Bathroom"/></td>
       </tr>
       <tr>
-       <td><img src="images/galleries/spaces/07_spc_thumb.jpg"
+       <td><img src="images/galleries/spaces/07_space_thumb.jpg"
                 alt="Nativity"/></td>
-       <td><img src="images/galleries/spaces/08_spc_thumb.jpg"
+       <td><img src="images/galleries/spaces/08_space_thumb.jpg"
                 alt="Evangelista sculpture"/></td>
       </tr>
     </table>
@@ -37,7 +43,7 @@
 <div id="right-content" class="grid_8 ">
   <div id="galleries_title">The Life in Death in Venice</div>
   <div id="big_img_one">
-    <img id="BigOne" src="images/galleries/spaces/01_spc.jpg" alt="Misericordia"/>
+    <img id="BigOne" src="images/galleries/spaces/01_space.jpg" alt="Misericordia"/>
   </div>
 </div>
 
