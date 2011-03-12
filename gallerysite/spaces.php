132d8115c39a16d72c6a5369a4b2e7fd9f6d4056
annaship <ashipunova3@gmail.com>
2011-03-12 10:43:48 -0500

    change images

--- gallerysite/spaces.php
+++ gallerysite/spaces.php
@@ -14,27 +14,27 @@
   </div>
     <table id="thumbs">
       <tr>
-       <td><img src="images/galleries/spaces/01_space_thumb.jpg"
+       <td><img src="images/galleries/spaces/space_01_thumb.jpg"
                 alt="Misericordia"/></td>
-       <td><img src="images/galleries/spaces/02_space_thumb.jpg"
+       <td><img src="images/galleries/spaces/space_02_thumb.jpg"
                 alt="Glass globes"/></td>
       </tr>
       <tr>
-       <td><img src="images/galleries/spaces/03_space_thumb.jpg"
+       <td><img src="images/galleries/spaces/space_03_thumb.jpg"
                 alt="Papafava"/></td>
-       <td><img src="images/galleries/spaces/04_space_thumb.jpg"
+       <td><img src="images/galleries/spaces/space_04_thumb.jpg"
                 alt="Arsenale"/></td>
       </tr>
       <tr>
-       <td><img src="images/galleries/spaces/05_space_thumb.jpg"
+       <td><img src="images/galleries/spaces/space_05_thumb.jpg"
                 alt="Troughs"/></td>
-       <td><img src="images/galleries/spaces/06_space_thumb.jpg"
+       <td><img src="images/galleries/spaces/space_06_thumb.jpg"
                 alt="Bathroom"/></td>
       </tr>
       <tr>
-       <td><img src="images/galleries/spaces/07_space_thumb.jpg"
+       <td><img src="images/galleries/spaces/space_07_thumb.jpg"
                 alt="Nativity"/></td>
-       <td><img src="images/galleries/spaces/08_space_thumb.jpg"
+       <td><img src="images/galleries/spaces/space_08_thumb.jpg"
                 alt="Evangelista sculpture"/></td>
       </tr>
     </table>
@@ -43,7 +43,7 @@
 <div id="right-content" class="grid_8 ">
   <div id="galleries_title">The Life in Death in Venice</div>
   <div id="big_img_one">
-    <img id="BigOne" src="images/galleries/spaces/01_space.jpg" alt="Misericordia"/>
+    <img id="BigOne" src="images/galleries/spaces/space_01.jpg" alt="Misericordia"/>
   </div>
 </div>
 
