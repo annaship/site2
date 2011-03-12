55f41a86b74ab4af2796d0e17ba0d2ce8a5e16ea
annaship <ashipunova3@gmail.com>
2011-03-12 12:55:08 -0500

    add left title to casalinghe, change top margin for thumbs

--- gallerysite/casalinghe.php
+++ gallerysite/casalinghe.php
@@ -6,40 +6,46 @@
 <div id="content" class="container_12">
 
 <div id="left-content" class="grid_4 ">
+  <div id="left-title">
+    <div id="left-title-big">
+    <span id="left_title_em">Casalinghe</span>:<br/>
+    Traditional Italian<br/> Housewives
+    </div>
+  </div>
   <table id="thumbs">
     <tr>
-      <td><img src="images/galleries/casalinghe/01_cas_thumb.jpg"
+      <td><img src="images/galleries/casalinghe/Casalinghe_01_thumb.jpg"
                alt="Antonia"/></td>
-      <td><img src="images/galleries/casalinghe/02_cas_thumb.jpg"
+      <td><img src="images/galleries/casalinghe/Casalinghe_02_thumb.jpg"
                alt="Carla"/></td>
     </tr>
     <tr>
-      <td><img src="images/galleries/casalinghe/03_cas_thumb.jpg"
+      <td><img src="images/galleries/casalinghe/Casalinghe_03_thumb.jpg"
                alt="Carla_hands"/></td>
-      <td><img src="images/galleries/casalinghe/04_cas_thumb.jpg"
+      <td><img src="images/galleries/casalinghe/Casalinghe_04_thumb.jpg"
                alt="Margherita tomatoes"/></td>
     </tr>
     <tr>
-      <td><img src="images/galleries/casalinghe/05_cas_thumb.jpg"
+      <td><img src="images/galleries/casalinghe/Casalinghe_05_thumb.jpg"
                alt="Anna&nbsp;&amp; Maria"/></td>
-      <td><img src="images/galleries/casalinghe/06_cas_thumb.jpg"
+      <td><img src="images/galleries/casalinghe/Casalinghe_06_thumb.jpg"
                alt="Giuliana"/></td>
     </tr>
     <tr>
-      <td><img src="images/galleries/casalinghe/07_cas_thumb.jpg"
+      <td><img src="images/galleries/casalinghe/Casalinghe_07_thumb.jpg"
                alt="Lidia Palanca"/></td>
-      <td><img src="images/galleries/casalinghe/08_cas_thumb.jpg"
+      <td><img src="images/galleries/casalinghe/Casalinghe_08_thumb.jpg"
                alt="Anna&nbsp;&amp; the Winning Lottery Ticket"/></td>
     </tr>
   </table>
 </div>
 
 <div id="right-content" class="grid_8 ">
-  <div id="galleries_title">Casalinghe:<br/>
-    Traditional Italian Housewives
+  <div id="galleries_title">
+    Venice&nbsp;&amp; the Secrets of Women: the Sibyls
   </div>
   <div id="big_img_one">
-    <img id="BigOne" src="images/galleries/casalinghe/01_cas.jpg" alt="Antonia"/>
+    <img id="BigOne" src="images/galleries/casalinghe/Casalinghe_01.jpg" alt="Antonia"/>
   </div>
 
 </div>
