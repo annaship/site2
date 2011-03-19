6c61d1a8d7b62dbfa027b1fb505e1c0875119275
annaship <ashipunova3@gmail.com>
2011-03-19 13:43:21 -0400

    copyright; centered right title

---
+++ gallerysite/misc/3_9_php_template/portfolio_casalinghe.php
@@ -0,0 +1,62 @@
+<?php
+  include_once 'template.php';
+  template_first_part('Venice&nbsp;&amp; the Secrets of Women:<br/>
+      <span class="italic_em">Casalinghe</span>&nbsp;&amp; Sibyls');
+
+        include('include/left_menu.html');
+
+  template_content("
+    <div id='right-text'>
+      <div id='right-text-title'>
+        Venice&nbsp;&amp; the Secrets of Women:<br/>
+        <span class='italic_em'>Casalinghe</span>&nbsp;&amp; Sibyls
+      </div>
+        <p>In the middle of my life I found myself, quite unlike Dante, in the middle of a vast and luminous lagoon dotted with floating little islands, each covered with airborne displays of women&#39;s work. The first thing a New England woman with a Puritan heritage coming to Italy is aware of is that she is in a culture where women are venerated. </p>
+        <p>With a year in Venice in front of me, I felt like Belle in Jean Cocteau&#39;s <span class='italic_em'>La Belle et la B&ecirc;te</span>: doors kept springing open in front of me, inviting me to experience I knew not what. </p>
+        <p>That year and during many subsequent sojourns in Venice, a desire grew in me to understand why older Italian women, (they were the repressed objects of the Male Gaze, house-arrested victims of the Patriarchy, were they not?), exulted in their own identities.</p>
+        <p>I got a Gladys Klebel Delmas Foundation grant to interview and videotape Venetian <span class='italic_em'>casalinghe</span>, traditional Italian housewives,&nbsp;&amp; I return to Venice each year and photograph them&nbsp;&amp; their art of hanging laundry.</p>
+        <p>A <span class='italic_em'>casalinga</span> is a woman who finds pleasure, meaning,&nbsp;&amp; purpose in creating a home that nourishes those whom she loves. She can also be a sibyl.</p>
+        <p>Sibyls were ancient Greek prophetesses who understood how the world works. Far from being evangelists, they kept their knowledge of the past, present,&nbsp;&amp; future in secret books, and they would impart their wisdom only to those who journeyed to seek it.  </p>
+        <p>From the Middle Ages on in Italy, the mythology of the Sibyl ripened&nbsp;&amp; deepened. She could be a mysterious sage like the Queen of Sheba, a seductive witch like Circe, a loving mother like Saint Anne, a wrinkled&nbsp;&amp; muscular virago like Michelangelo&#39;s Cumaean Sibyl on the Sistine ceiling. </p>
+        <p>Sibyls always had a story to tell, whether or not anyone was listening. In the realm of the psyche, their power was immense. In the public sphere they were often invisible.</p>
+        <p>This portfolio is about some Venetian Sibyls.</p>
+        <p>For more about symbolic female power and its connection with hung fabric, please visit my <a href='http://www.pedlosky.org/manifoldlaundryhome.html target='_blank'>Manifold Laundry</a> web pages.</p>
+
+    </div>
+  ");
+
+?>
+
+<!--<div id="content" class="container_12">
+
+<div id="left-content" class="grid_4">
+  <?php
+//    include("include/left_menu.html");
+  ?>
+</div>
+
+<div id="right-content" class="grid_8">
+  <div id="right-text">
+    <div id="right-text-title">
+      Venice&nbsp;&amp; the Secrets of Women:<br/>
+      <span class="italic_em">Casalinghe</span>&nbsp;&amp; Sibyls
+    </div>
+      <p>In the middle of my life I found myself, quite unlike Dante, in the middle of a vast and luminous lagoon dotted with floating little islands, each covered with airborne displays of women&#39;s work. The first thing a New England woman with a Puritan heritage coming to Italy is aware of is that she is in a culture where women are venerated. </p>
+      <p>With a year in Venice in front of me, I felt like Belle in Jean Cocteau&#39;s <span class="italic_em">La Belle et la B&ecirc;te</span>: doors kept springing open in front of me, inviting me to experience I knew not what. </p>
+      <p>That year and during many subsequent sojourns in Venice, a desire grew in me to understand why older Italian women, (they were the repressed objects of the Male Gaze, house-arrested victims of the Patriarchy, were they not?), exulted in their own identities.</p>
+      <p>I got a Gladys Klebel Delmas Foundation grant to interview and videotape Venetian <span class="italic_em">casalinghe</span>, traditional Italian housewives,&nbsp;&amp; I return to Venice each year and photograph them&nbsp;&amp; their art of hanging laundry.</p>
+      <p>A <span class="italic_em">casalinga</span> is a woman who finds pleasure, meaning,&nbsp;&amp; purpose in creating a home that nourishes those whom she loves. She can also be a sibyl.</p>
+      <p>Sibyls were ancient Greek prophetesses who understood how the world works. Far from being evangelists, they kept their knowledge of the past, present,&nbsp;&amp; future in secret books, and they would impart their wisdom only to those who journeyed to seek it.  </p>
+      <p>From the Middle Ages on in Italy, the mythology of the Sibyl ripened&nbsp;&amp; deepened. She could be a mysterious sage like the Queen of Sheba, a seductive witch like Circe, a loving mother like Saint Anne, a wrinkled&nbsp;&amp; muscular virago like Michelangelo&#39;s Cumaean Sibyl on the Sistine ceiling. </p>
+      <p>Sibyls always had a story to tell, whether or not anyone was listening. In the realm of the psyche, their power was immense. In the public sphere they were often invisible.</p>
+      <p>This portfolio is about some Venetian Sibyls.</p>
+      <p>For more about symbolic female power and its connection with hung fabric, please visit my <a href="http://www.pedlosky.org/manifoldlaundryhome.html target='_blank">Manifold Laundry</a> web pages.</p>
+
+  </div>
+</div>
+
+</div>-->
+
+<?php
+  template_last_part()
+?>
