bbc74c41a9eb800ffac4249106350147f722fb7b
annaship <ashipunova3@gmail.com>
2011-03-26 23:08:03 -0400

    global var; intermediate

--- gallerysite/scripts.js
+++ gallerysite/scripts.js
@@ -1,77 +1,7 @@
-//function highlightLink()
-//{
-//  var i, menu_tag, thisPage, my_obj;
-//  menu_tag = 'navigation';
-////  menu_tag = 'bg_menu';
-////Get filename from the URL
-//	thisPage = document.URL.substring(document.URL.lastIndexOf("/")+1, document.URL.length);
-//  my_obj   = document.getElementById(menu_tag).getElementsByTagName('a');
-//  for (i=0; i<my_obj.length; i++ )
-//	{
-//		//Get only the filename from the href of navigation link
-//		pageLink = my_obj[i].href.substring(my_obj[i].href.lastIndexOf("/")+1,
-//      my_obj[i].href.length);
-//		if (thisPage == pageLink)
-//		{
-//			my_obj[i].className = "current";
-//		}
-//	}
-//}
-
-function init()
-{
-    if (document.addEventListener)
-    {
-       document.addEventListener("keydown",keydown,false);
-       document.addEventListener("keypress",keypress,false);
-       document.addEventListener("keyup",keyup,false);
-       document.addEventListener("textInput",textinput,false);
-    }
-    else if (document.attachEvent)
-    {
-       document.attachEvent("onkeydown", keydown);
-       document.attachEvent("onkeypress", keypress);
-       document.attachEvent("onkeyup", keyup);
-       document.attachEvent("ontextInput", textinput);
-    }
-    else
-    {
-       document.onkeydown= keydown;
-       document.onkeypress= keypress;
-       document.onkeyup= keyup;
-       document.ontextinput= textinput;   // probably doesn't work
-    }
-alert("init");
-    document.testform.t.value+= '';
-    lines= 0;
-}
-
-
- function addEvent(elm, evType, fn, useCapture)
- // addEvent and removeEvent
- // cross-browser event handling for IE5+,  NS6 and Mozilla
- // By Scott Andrew
- {
-   if (elm.addEventListener){
-    elm.addEventListener(evType, fn, useCapture);
-    return true;
-   } else if (elm.attachEvent){
-    var r = elm.attachEvent("on"+evType, fn);
-    return r;
-   } else {
-    alert("Handler could not be removed");
-   }
- }  
-
-function showsrc(thumb){
-  alert(thumb.src.replace("http://localhost/gallerysite/images/galleries/",""));
-  thumb.style.border='1px solid';
-
-}
+var myVar;
 
 function change_big_one(thumb){
   thumb.style.border='0px';
-
   document.getElementById('BigOne').src=thumb.src.replace("_thumb","");
   document.getElementById('BigOne').alt=thumb.alt;
   document.getElementById("big-img-title").innerHTML = thumb.title;
@@ -88,31 +18,9 @@ function findimg()
     }
     my_obj[i].onclick = function() {
       showsrc(this);
-//      alert("UUU");
-//      alert(my_obj[i].src);
-//      KeyCheck();
     }
-
-//    my_obj[i].onclick = function() {
-//      showresult(event);
-//    }
-//    my_obj[i].onKeyUp     = alert("U");
-//    my_obj[i].onKeyUp     = alert(event.keyCode);
   }
-//      function() {showresult(event);}
-//	   <input type="text" onKeyUp="keyWasPressed(event)">
-//    <script>function keyWasPressed(evt){ alert(evt.keyCode) }</script>
 
-
-//  TODO: change alt and title dinamically
-//  for IE need to do selector
-  //  var sels = document.getElementsByTagName("SELECT");
-//for(var i = 0, maxI = sels.length; i < maxI; ++i) {
-//  var sel = sels[i];
-//  if(sel == slctFild)
-//    continue;  // exclude the current select element
-//  sel.selectedIndex = 0;
-//}
 }
 
 //function showresult(e)
@@ -142,8 +50,16 @@ function findimg()
 //    break;
 //  }
 //}
+
+
+function showsrc(thumb){
+  alert(thumb.src.replace("http://localhost/gallerysite/images/galleries/",""));
+  thumb.style.border='1px solid';
+}
+
 function KeyCheck() {
-  alert(event.keyCode);
+  myVar = event.keyCode
+  alert(myVar);
 }
 
 //function clickObj() {
