1cd289a2bcd4c14076df78a49cfce0adffa8b27c
annaship <ashipunova3@gmail.com>
2011-03-27 01:05:45 -0400

    down works

--- gallerysite/scripts.js
+++ gallerysite/scripts.js
@@ -102,10 +102,11 @@ function KeyCheck() {
       thumb = thumbObj.parentNode.parentNode.previousSibling.previousSibling.childNodes[1].firstChild;
 //     alert(thumbObj.parentNode.parentNode.previousSibling.previousSibling.childNodes[1].firstChild.attributes[1].value);
       if (thumbObj.parentNode.nextSibling.nodeType == 1) {
-//        thumb = thumbObj.parentNode.nextSibling.firstChild;
-//      }
-//      else {
-//        thumb = thumbObj.parentNode.nextSibling.nextSibling.firstChild;
+        thumb = thumbObj.parentNode.previousSibling.childNodes[0].firstChild;
+//        todo: check in IE
+      }
+      else {
+        thumb = thumbObj.parentNode.parentNode.previousSibling.previousSibling.childNodes[1].firstChild;
       }
       change_big_one(thumb);
     }
@@ -113,16 +114,47 @@ function KeyCheck() {
     if (keyValue == 38) {
     if (imgNumber > 2 && isEven(imgNumber)) {
 //     alert(thumbObj.parentNode.parentNode.previousSibling.previousSibling.childNodes[3].childNodes[0].attributes[1].value);
-     thumb = thumbObj.parentNode.parentNode.previousSibling.previousSibling.childNodes[3].childNodes[0];
-//      if (thumbObj.parentNode.nextSibling.nodeType == 1) {
+      if (thumbObj.parentNode.nextSibling.nodeType == 1) {
 //        thumb = thumbObj.parentNode.nextSibling.firstChild;
-//      }
-//      else {
+        thumb = thumbObj.parentNode.previousSibling.childNodes[1].firstChild;
+      }
+      else {
 //        thumb = thumbObj.parentNode.nextSibling.nextSibling.firstChild;
-//      }
+        thumb = thumbObj.parentNode.parentNode.previousSibling.previousSibling.childNodes[3].firstChild;
+      }
       change_big_one(thumb);
     }
   }
+  // down
+      if (keyValue == 40) {
+    if (imgNumber < 7 && !isEven(imgNumber)) {
+      thumb = thumbObj.parentNode.parentNode.nextSibling.nextSibling.childNodes[1].firstChild;
+//     alert(thumbObj.parentNode.parentNode.nextSibling.nextSibling.childNodes[1].firstChild.attributes[1].value);
+      if (thumbObj.parentNode.nextSibling.nodeType == 1) {
+        thumb = thumbObj.parentNode.nextSibling.childNodes[0].firstChild;
+//        todo: check in IE
+      }
+      else {
+        thumb = thumbObj.parentNode.parentNode.nextSibling.nextSibling.childNodes[1].firstChild;
+      }
+      change_big_one(thumb);
+    }
+  }
+    if (keyValue == 40) {
+    if (imgNumber < 7 && isEven(imgNumber)) {
+//     alert(thumbObj.parentNode.parentNode.nextSibling.nextSibling.childNodes[3].childNodes[0].attributes[1].value);
+      if (thumbObj.parentNode.nextSibling.nodeType == 1) {
+//        thumb = thumbObj.parentNode.nextSibling.firstChild;
+        thumb = thumbObj.parentNode.nextSibling.childNodes[1].firstChild;
+      }
+      else {
+//        thumb = thumbObj.parentNode.nextSibling.nextSibling.firstChild;
+        thumb = thumbObj.parentNode.parentNode.nextSibling.nextSibling.childNodes[3].firstChild;
+      }
+      change_big_one(thumb);
+    }
+  }
+
 }
 
 //12
