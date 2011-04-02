f1779f9a8402ace3c4710acb56e87399ca792ac5
annaship <ashipunova3@gmail.com>
2011-04-02 06:30:42 -0400

    add openings, inremediate

--- gallerysite/openings.php
+++ gallerysite/openings.php
@@ -8,53 +8,62 @@
 <div id="left-content" class="grid_4 ">
   <div id="left-title">
     <div id="left-title-big">
-    <span id="left_title_em">Casalinghe</span>:<br/>
-    Traditional Italian<br/> Housewives
+    <span id="left_title_em">Panoramas</span>
     </div>
   </div>
   <table id="thumbs">
     <tr>
       <td><img src="images/galleries/openings/openings_01_thumb.jpg"
-               title="Margherita, Sant&#39;Erasmo, Venezia"
-               alt="Margherita"/></td>
+               title="Santa Croce, Venezia"
+               alt="Rooftops"
+              /></td>
       <td><img src="images/galleries/openings/openings_02_thumb.jpg"
-               title="Anna&nbsp;&amp; Maria, Giudecca, Venezia"
-               alt="Anna&nbsp;&amp; Maria"/></td>
+               title="Campo dell&#8217;Abbazia, Venezia"
+               alt="Couple in campo"
+              /></td>
     </tr>
     <tr>
       <td><img src="images/galleries/openings/openings_03_thumb.jpg"
-               title="Amelia, Dorsoduro, Venezia"
-               alt="Amelia"/></td>
+               title="Church of Mary Magdalen, Venezia"
+               alt="Mary Magdalen&#8217;s church"
+							/></td>
       <td><img src="images/galleries/openings/openings_04_thumb.jpg"
-               title="Venetian Sibyl"
-               alt="Anna"/></td>
+               title="San Giovanni Evangelista, Venezia"
+               alt="St. John the Baptist church "
+							/></td>
     </tr>
     <tr>
       <td><img src="images/galleries/openings/openings_05_thumb.jpg"
-               title="Antonia, Santa Croce, Venezia"
-               alt="Antonia"/></td>
+               title="Campo Bold&ograve;, Dorsoduro, Venezia"
+               alt="Campo, Dorsoduro"
+							/></td>
       <td><img src="images/galleries/openings/openings_06_thumb.jpg"
-               title="Carla, Vezio"
-               alt="Carla"/></td>
+               title="Island of the Vignole, Venetian Lagoon"
+               alt="Fishing net"
+							/></td>
     </tr>
     <tr>
       <td><img src="images/galleries/openings/openings_07_thumb.jpg"
-               title="Lidia, La Giudecca, Venezia"
-               alt="Lidia"/></td>
+               title="Trattoria, Venetian Lagoon"
+               alt="Trattoria"
+							/></td>
       <td><img src="images/galleries/openings/openings_08_thumb.jpg"
-               title="The Winning Lottery Ticket"
-               alt="Anna"/></td>
+               title="Private Garden, Venetian Lagoon"
+               alt="Private garden"
+              /></td>
     </tr>
   </table>
 </div>
 
 <div id="right-content" class="grid_8 ">
   <div id="galleries_title">
-    Venice&nbsp;&amp; the Secrets of Women: the Sibyls
+    Venetian Spaces: Openings
   </div>
   <div id="big_img_one">
-    <img id="BigOne" src="images/galleries/openings/openings_01.jpg" alt="Margherita"/>
-    <div id="big-img-title">Margherita, Sant&#39;Erasmo, Venezia</div>
+    <img id="BigOne" src="images/galleries/openings/openings_01.jpg"
+               alt="Rooftops"
+         />
+    <div id="big-img-title">Santa Croce, Venezia</div>
   </div>
 
 </div>
