af9cbfc5156e487e8604909c6b2ee61d97f65138
annaship <ashipunova3@gmail.com>
2011-04-15 19:17:23 -0400

    galeggianti

--- gallerysite/openings.php
+++ gallerysite/openings.php
@@ -8,45 +8,47 @@
 <div id="left-content" class="grid_4 ">
   <div id="left-title">
     <div id="left-title-big">
-    <span id="left_title_em">Panoramas</span>
+<!--    <span id="left_title_em">-->
+      Panoramas
+<!--    </span>-->
     </div>
   </div>
   <table id="thumbs">
     <tr>
-      <td><img src="images/galleries/openings/openings_01_thumb.png"
+      <td><img src="images/galleries/openings/openings_01_thumb.jpg"
                title="Santa Croce, Venezia"
                alt="Rooftops"
               /></td>
-      <td><img src="images/galleries/openings/openings_02_thumb.png"
+      <td><img src="images/galleries/openings/openings_02_thumb.jpg"
                title="Campo dell&#8217;Abbazia, Venezia"
                alt="Couple in campo"
               /></td>
     </tr>
     <tr>
-      <td><img src="images/galleries/openings/openings_03_thumb.png"
+      <td><img src="images/galleries/openings/openings_03_thumb.jpg"
                title="Church of Mary Magdalen, Venezia"
                alt="Mary Magdalen&#8217;s church"
 							/></td>
-      <td><img src="images/galleries/openings/openings_04_thumb.png"
+      <td><img src="images/galleries/openings/openings_04_thumb.jpg"
                title="San Giovanni Evangelista, Venezia"
                alt="St. John the Baptist church "
 							/></td>
     </tr>
     <tr>
-      <td><img src="images/galleries/openings/openings_05_thumb.png"
+      <td><img src="images/galleries/openings/openings_05_thumb.jpg"
                title="Trattoria, Venetian Lagoon"
                alt="Trattoria"							/></td>
-      <td><img src="images/galleries/openings/openings_06_thumb.png"
+      <td><img src="images/galleries/openings/openings_06_thumb.jpg"
                title="Campo Bold&ograve;, Dorsoduro, Venezia"
                alt="Campo, Dorsoduro"
 							/></td>
     </tr>
     <tr>
-      <td><img src="images/galleries/openings/openings_07_thumb.png"
+      <td><img src="images/galleries/openings/openings_07_thumb.jpg"
                title="Island of the Vignole, Venetian Lagoon"
                alt="Fishing net"
 							/></td>
-      <td><img src="images/galleries/openings/openings_08_thumb.png"
+      <td><img src="images/galleries/openings/openings_08_thumb.jpg"
                title="Private Garden, Venetian Lagoon"
                alt="Private garden"
               /></td>
@@ -59,7 +61,7 @@
     Venetian Spaces: Openings
   </div>
   <div id="big_img_one">
-    <img id="BigOne" src="images/galleries/openings/openings_01.png"
+    <img id="BigOne" src="images/galleries/openings/openings_01.jpg"
                alt="Rooftops"
          />
     <div id="big-img-title">Santa Croce, Venezia</div>
